ea87913f6eea80b11dc19167e2e4672809af7a5c
Axel <axel.andrade>
2020-05-09 00:39:53 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'catalog',
+    define: {
+        timestamp: true,
+        underscored: true
+    },
+};
---
+++ src/database/migrations/20200509032605-create-movies.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('movies', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      synopsis: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      language: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('movies');
+  }
+};
